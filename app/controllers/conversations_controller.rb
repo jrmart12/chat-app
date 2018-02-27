187c73a70a2aaa87dd1d2ceeaf655149db0838d2
jrmart12 <rodolfojose@live.com>
2018-02-27 01:14:26 -0600

    arreglando edit user

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -18,36 +18,10 @@ class ConversationsController < ApplicationController
   def show
   end
 
-  def mark_as_read
-    @conversation.mark_as_read(current_user)
-    flash[:success] = 'The conversation was marked as read.'
-    redirect_to conversations_path
-  end
-
   def reply
     current_user.reply_to_conversation(@conversation, params[:body])
     flash[:success] = 'Reply sent'
     redirect_to conversation_path(@conversation)
-  end
-
-  def destroy
-    @conversation.move_to_trash(current_user)
-    flash[:success] = 'The conversation was moved to trash.'
-    redirect_to conversations_path
-  end
-
-  def restore
-    @conversation.untrash(current_user)
-    flash[:success] = 'The conversation was restored.'
-    redirect_to conversations_path
-  end
-
-  def empty_trash
-    @mailbox.trash.each do |conversation|
-      conversation.receipts_for(current_user).update_all(deleted: true)
-    end
-    flash[:success] = 'Your trash was cleaned!'
-    redirect_to conversations_path
   end
 
   private
